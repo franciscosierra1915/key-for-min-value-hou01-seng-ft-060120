295e8df69057d6ba490a3bd563e3773872ac4051
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-15 18:40:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   low_key = nil 
   low_value = nil 
-  name.hash.each do |key, value|
+  name_hash.each do |key, value|
     if low_value == nil || value < low_value 
       low_value = value 
       low_key = key 
